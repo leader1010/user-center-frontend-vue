e26fa669d12b3b6226709dac8131a89b8baa5df8
leader <930327366@qq.com>
2023-05-01 20:31:54 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="login-container">
+
+
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/router.js
@@ -0,0 +1,12 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    linkActiveClass: 'acitvite-router',
+    routes: [{}, {}]
+})
+
+export default router
+
+
+
